ed2aea68acff51555b3dc3af2ff4abee9d73fc01
Your Name <bonacchigerman@gmail.com>
2022-04-27 15:51:51 -0300

    add hashchange and oldItems

--- KI-367339.js
+++ KI-367339.js
@@ -1,31 +1,43 @@
+function observeChangesOnCartItems(oldItems) {
+  const cartItems = document.querySelector("ul.cart-items");
+
+  const observer = new MutationObserver((mutationsList, observer) => {
+    const items = cartItems.querySelectorAll("li");
+    //console.log(oldItems);
+    //console.log("items", items.length);
+    if (oldItems != items.length) {
+      const uniqueItems = [];
+      items.forEach((item) => {
+        const itemId = item.getAttribute("data-sku");
+        if (!uniqueItems.includes(itemId)) {
+          uniqueItems.push(itemId);
+        } else {
+          item.remove();
+        }
+      });
+      oldItems = uniqueItems.length;
+      //console.log("uniqueItems", uniqueItems.length);
+    }
+    
+  });
+
+  observer.observe(cartItems, {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  });
+}
+
 $(document).ready(function () {
+  let oldItems = -1
   const cartItems = document.querySelector("ul.cart-items");
 
   const observerCartItems = new MutationObserver((mutations, obsCI) => {
     if (document.contains(cartItems)) {
-      const observer = new MutationObserver((mutationsList, observer) => {
-        const items = cartItems.querySelectorAll("li");
-        console.log("items", items.length);
-
-        const uniqueItems = [];
-        items.forEach((item) => {
-          const itemId = item.getAttribute("data-sku");
-          if (!uniqueItems.includes(itemId)) {
-            uniqueItems.push(itemId);
-          } else {
-            item.remove();
-          }
-        });
-
-        console.log("uniqueItems", uniqueItems.length);
+      observeChangesOnCartItems(oldItems);
+      window.addEventListener("hashchange", () => {
+        observeChangesOnCartItems(oldItems);
       });
-
-      observer.observe(cartItems, {
-        attributes: true,
-        childList: true,
-        subtree: true,
-      });
-
       obsCI.disconnect();
       return;
     }
